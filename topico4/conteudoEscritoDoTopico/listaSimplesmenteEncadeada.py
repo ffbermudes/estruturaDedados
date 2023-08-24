ec65cd719bfe48c1772c7118f4528bb8387d224c
ffbermudes19 <ffbermudes19@gmail.com>
2023-08-24 00:23:34 -0300

    att

--- topico4/conteudoEscritoDoTopico/listaSimplesmenteEncadeada.py
+++ topico4/conteudoEscritoDoTopico/listaSimplesmenteEncadeada.py
@@ -2,7 +2,7 @@ from nodoClass import Node
 
 class ListaSimplesmenteEncadeada:
 
-    def __init__(self, nodo = 0) -> None:
+    def __init__(self, nodo = None) -> None:
         self.inicio = nodo
         self.tamanhoDaLista = 0
 
