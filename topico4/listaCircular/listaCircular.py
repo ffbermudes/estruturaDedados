d258e8f9fd65802477071e4d5692b50878872fda
ffbermudes <ffbermudes19@gmail.com>
2023-08-28 19:19:31 -0300

    Exemplos lista circular

--- topico4/listaCircular/listaCircular.py
+++ topico4/listaCircular/listaCircular.py
@@ -4,8 +4,9 @@ class CircularList:
     def __init__(self, default = None) -> None:
         self.inicio = default
         self.length = 0
+
     def insereNoInicio(self, dado):
-        novoNodo = Node(dado)
+        novoNodo = Node(dado) #Node -> {dado, proximo}
         nodoTemporario = self.inicio
         novoNodo.proximo = self.inicio
         if self.inicio is not None:
@@ -16,6 +17,3 @@ class CircularList:
             novoNodo.proximo = novoNodo
         self.inicio = novoNodo
         self.length += 1
-
-    def imprimeAllDatas(self):
-        allData = self.inicio.dado #last element
