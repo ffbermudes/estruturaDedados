bc478c4adf149d6ebfb4b31d180c8c7d167fa76d
ffbermudes19 <ffbermudes19@gmail.com>
2023-08-29 09:57:31 -0300

    classe Nodo AOP2

--- AOP2/no.py
+++ AOP2/no.py
@@ -1,3 +1,10 @@
 class Nodo:
-  def __init__(self) -> None:
-		pass
+	def __init__(self, dado = 0, proximoNodo = None):
+		self.dado = dado
+		self.proximo = proximoNodo
+
+	def __repr__(self):
+		return f"{self.dado} -> {self.proximo}"
+
+	def setProximo(self, proximoNodo):
+		self.proximo = Nodo(proximoNodo)
