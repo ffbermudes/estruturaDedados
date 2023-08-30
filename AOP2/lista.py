476398b50d0ff8760edc28900c319dc1dd633cab
ffbermudes19 <ffbermudes19@gmail.com>
2023-08-30 14:12:28 -0300

    corrigido função de calculo de média

--- AOP2/lista.py
+++ AOP2/lista.py
@@ -32,10 +32,10 @@ class ListaEncadeada:
 		totGastos = 0
 		tamanhoLista = self.tamanhoDaLista
 		while current_node.proximo != None:
-			totGastos += current_node.dado.valorDaConta
+			totGastos += current_node.valorDaConta
 			print(totGastos)
-			print(current_node.dado)
+			print(current_node)
 			current_node = current_node.proximo
-		totGastos += current_node.dado.valorDaConta
+		totGastos += current_node.valorDaConta
 		media = totGastos/tamanhoLista
 		return f"A media de gastos do cliente é: R$ {media:.2f}"
